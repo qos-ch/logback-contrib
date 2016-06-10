f565605b29bec5888f7c9224bb4df17394d2c9c4
Tony Trinh <tony19@gmail.com>
2016-06-10 16:29:46 -0500

    Specify release-sign-artifacts profile in release.sh
    The artifacts are only signed when the `release-sign-artifacts`
    profile is activated, which is normally done by the maven-release-plugin
    for the release:perform task. That's not happening for some reason, so
    as a workaround, we're now explicitly setting the profile when using the
    release script. We're soon switching to Gradle, so no further effort will
    be put in to fix this in Maven configs.
    
    This patch also allows passing mvn arguments to release.sh, which is
    useful for setting -DdryRun=true.

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 # Deploy release builds to Sonatype (and not the staging repo)
-mvn -Prelease release:clean release:prepare release:perform
+mvn $1 -Prelease -Prelease-sign-artifacts release:clean release:prepare release:perform
 
 
 # To redeploy without touching git (tags, commits, etc), run:
